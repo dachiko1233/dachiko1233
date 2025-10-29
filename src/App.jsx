5288ce3500052b80d9c1b9c58f9d870c769c7aac
dachiko <dachimari9@gmail.com>
2025-10-29 18:15:31 +0200

    new project

--- src/App.jsx
+++ src/App.jsx
@@ -325,6 +325,7 @@ function MoviesDetals({ selectedId, onCloseMovie, onAddWatch, watched }) {
     };
 
     onAddWatch(newWatchedMovie);
+    onCloseMovie();
   }
 
   useEffect(() => {
@@ -393,8 +394,6 @@ function MoviesDetals({ selectedId, onCloseMovie, onAddWatch, watched }) {
     </div>
   );
 }
-
-function Gio() {}
 
 function WatchedSummery({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
