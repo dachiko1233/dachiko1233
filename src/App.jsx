744da2bdcc22fc4192864c777aa5d8c2225bcda0
dachiko <dachimari9@gmail.com>
2025-11-10 11:37:21 +0200

    New project

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,7 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+    localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatch(id) {
